345a32e027df7b2bfa01a40234eb07721ee7ed8b
김시윤 <74858992+Im-Siyoun@users.noreply.github.com>
2021-06-25 07:44:59 +0900

    fix: remove font dir

--- crawling/wordcloud.py
+++ crawling/wordcloud.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 
 def cloud(text):
-    wordcloud = WordCloud(font_path='C:\\Users\\rlagu\\Desktop\\폰트\\고딕체\\NIXGONFONTS B 2.0.ttf', background_color='white').generate(text)
-    plt.figure(figsize=(30,30)) #이미지 사이즈 지정
-    plt.imshow(wordcloud, interpolation='lanczos') #이미지의 부드럽기 정도
-    plt.axis('off') #x y 축 숫자 제거
+    wordcloud = WordCloud(font_path='', background_color='white').generate(text)
+    plt.figure(figsize=(30,30))
+    plt.imshow(wordcloud, interpolation='lanczos')
+    plt.axis('off')
     img = BytesIO()
     plt.savefig(img, format='png', dpi=800)
     wordcloud.to_file('wordcloud.png')
